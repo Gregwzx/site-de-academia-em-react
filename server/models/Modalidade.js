fe899884faa1075c300b48342c4f8df4dadbb239
Jailson Ferreira <jailsonjuniorcom12@gmail.com>
2025-11-08 13:56:03 -0300

    Delete server directory

--- server/models/Modalidade.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const ModalidadeSchema = new mongoose.Schema({
-  nome: String,
-  descricao: String,
-  imagem: String
-}, { timestamps: true });
-
-export default mongoose.model('Modalidade', ModalidadeSchema);
